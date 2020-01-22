064d7fd8233e54442f6062fa0a130e1ce073edf3
Daniella Santiago <danii.santiago@gmail.com>
2020-01-22 02:48:12 -0300

    implementando update do department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -10,22 +10,29 @@ public class Program2 {
 
 	public static void main(String[] args) {
 		
-		DepartmentDao dep = DaoFactory.createDepartmentDao();
+		DepartmentDao depDao = DaoFactory.createDepartmentDao();
 		
 		System.out.println("==== TEST 1: department findAll =====");
-		List<Department> list = dep.findAll();
+		List<Department> list = depDao.findAll();
 		list.forEach(System.out::println);
 		
 		System.out.println();
 		System.out.println("==== TEST 2: department findById =====");
-		Department department = dep.findById(2);
+		Department department = depDao.findById(2);
 		System.out.println(department);
 		
 		System.out.println();
 		System.out.println("==== TEST 3: department insert =====");
 		Department newDep = new Department(null, "Reality");
-		dep.insert(newDep);
+		depDao.insert(newDep);
 		System.out.println("Inserted! New id = " + newDep.getId());
+		
+		System.out.println();
+		System.out.println("==== TEST 4: department update =====");
+		department = depDao.findById(5);
+		department.setName("Shows");
+		depDao.update(department);
+		System.out.println("Update completed");
 		
 	}
 }
